41c0c2e053adb5dc3082c66dda55c6a1efd5782a
LaurenAtkinson <52724335+LaurenAtkisnon@users.noreply.github.com>
2021-09-26 15:05:39 -0400

    Lauren's

--- routes/users/laatkinson.js
+++ routes/users/laatkinson.js
@@ -1,12 +1,19 @@
 module.exports = function(router, username) {
   router.get('/' + username, function(req, res, next) {
     res.render(
-      username + '/base', 
+      username + '/base',
       {
          title: username + '\'s Special Page',
          username: username
       });
   });
-
   // Set up additional routes here
+  router.get('/laatkinson/newPage', function(req, res, next) {
+    res.render(
+      username + '/newPage',
+      {
+         title: username + '\'s New Page',
+         username: username
+      });
+  });
 };
