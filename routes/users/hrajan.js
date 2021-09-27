103033e2cb6f0e621eeefd5ece2e4f6f1fdf37ca
Hephzibah Rajan <rajanhephzibah@gmail.com>
2021-09-27 01:59:47 -0400

    Assignment 4.1

--- routes/users/hrajan.js
+++ routes/users/hrajan.js
@@ -1,12 +1,20 @@
 module.exports = function(router, username) {
   router.get('/' + username, function(req, res, next) {
     res.render(
-      username + '/base', 
+      username + '/base',
       {
-         title: username + '\'s Special Page',
+         title: 'Hephzibah Rajan\'s Special Page',
          username: username
       });
   });
 
   // Set up additional routes here
+  router.get('/' + username + '/background', function(req, res, next) {
+    res.render(
+      username + '/background',
+      {
+         title: 'My Background',
+         username: username
+      });
+  });
 };
