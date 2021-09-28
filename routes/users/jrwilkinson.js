06570bd1237960ddc4a5117e120ac80fcbab1014
Julia <jrwilkinson@quinnipiac.edu>
2021-09-27 21:31:47 -0400

    jrwilkinson pages

--- routes/users/jrwilkinson.js
+++ routes/users/jrwilkinson.js
@@ -1,12 +1,20 @@
-module.exports = function(router, username) {
-  router.get('/' + username, function(req, res, next) {
+module.exports = function (router, username) {
+  router.get('/' + username, function (req, res, next) {
     res.render(
-      username + '/base', 
+      username + '/base',
       {
-         title: username + '\'s Special Page',
-         username: username
+        title: username + '\'s Main Page',
+        username: username
       });
   });
 
   // Set up additional routes here
+  router.get('/jrwilkinson/pgpg', function (req, res, next) {
+    res.render(
+      username + '/pgpg',
+      {
+        title: username + '\'s Page',
+        username: username
+      });
+  });
 };
